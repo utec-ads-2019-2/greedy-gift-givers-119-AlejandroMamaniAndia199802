8a30c0792bc131766e9c36c1e6504277d7e2cfce
Alejandro1998elebebecito <49255376+Alejandro1998elebebecito@users.noreply.github.com>
2019-09-24 17:16:09 -0500

    Uva accepted

--- main.cpp
+++ main.cpp
@@ -1,7 +1,47 @@
 #include <iostream>
-
+#include <string>
+#include <map>
 
 using namespace std;
-int main(){
 
+map< string, int> money;
+string name[15];
+
+int main ()
+{
+    bool printBlankLine = false;
+    int n,  cashGive, nReceivers;
+    string giver, receiver;
+    while (cin >> n)
+    {
+        if (printBlankLine)
+            cout << endl;
+        else
+            printBlankLine = true;
+
+        money.clear();
+        for (int i = 0; i < n; i++)
+        {
+            cin >> name[i];
+            money[name[i]] = 0;
+        }
+
+        for (int i = 1; i <= n; i++)
+        {
+            cin >> giver >> cashGive >> nReceivers;
+            for (int j = 1; j <= nReceivers; j++)
+            {
+                cin >> receiver;
+                money[receiver] += (cashGive / nReceivers);
+            }
+
+            if (nReceivers)
+                money[giver] -= (cashGive / nReceivers * nReceivers);
+        }
+
+        for (int i = 0; i < n; i++)
+            cout << name[i] << " " << money[name[i]] << endl;
+    }
+
+    return 0;
 }
