9425c879bbd073e929f2f280da6b91f439877bc1
Alejandro1998elebebecito <49255376+Alejandro1998elebebecito@users.noreply.github.com>
2019-09-24 16:30:08 -0500

    Add structure

---
+++ cmake-build-debug/CMakeFiles/greedy-gift-givers-119-AlejandroMamaniAndia199802.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/greedy-gift-givers-119-AlejandroMamaniAndia199802.dir/main.cpp.obj"
+  "greedy-gift-givers-119-AlejandroMamaniAndia199802.pdb"
+  "greedy-gift-givers-119-AlejandroMamaniAndia199802.exe"
+  "greedy-gift-givers-119-AlejandroMamaniAndia199802.exe.manifest"
+  "libgreedy-gift-givers-119-AlejandroMamaniAndia199802.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/greedy-gift-givers-119-AlejandroMamaniAndia199802.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
